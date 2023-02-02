89155bebc0d1b6fa321c6608e78ce03aeccf7cac
rex <rexardiente09@gmail.com>
2023-02-03 02:01:59 +0800

    Add ZetaMiner Project

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+(() => {
+  // load modal html
+  $(document).ready(() => {
+    $("#zetaminer").load("/assets/modals/zetaminer.html");
+    $("#innominer").load("/assets/modals/innominer.html");
+    $("#coinica").load("/assets/modals/coinica.html");
+    $("#wowooexchange").load("/assets/modals/wowooexchange.html");
+  });
+})();
